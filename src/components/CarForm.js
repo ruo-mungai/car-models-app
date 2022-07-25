043cd96765393590fcd2b2db5eaccd929217e7bf
Patrick <ruomungai@gmail.com>
2022-07-25 14:28:29 +0300

    adding Carform

--- src/components/CarForm.js
+++ src/components/CarForm.js
@@ -1,9 +1,16 @@
 import React from 'react'
-import TextField from '@mui/material/TextField';
-
+import TextField from '@mui/material/TextField'
+import Button from '@mui/material/Button';
 function CarForm() {
   return (
-    <div>CarForm</div>
+    <div>
+        <h2>Add Car Form</h2>
+        <TextField id="filled-basic" label="Make" variant="filled" />
+        <TextField id="filled-basic" label="Model" variant="filled" />
+        <TextField id="filled-basic" label="Origin" variant="filled" />
+        <TextField id="filled-basic" label="Year" variant="filled" />
+        <Button variant="contained" color="secondary"> Add Car</Button>
+    </div>
   )
 }
 
