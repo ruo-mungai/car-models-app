02f5d5d1918ec7d423d25bcf35d68d9ebcf6a728
Patrick <ruomungai@gmail.com>
2022-07-28 14:28:34 +0300

    to heruku

--- src/components/DisplayCars.js
+++ src/components/DisplayCars.js
@@ -1,12 +1,6 @@
 import React from 'react'
-import { withStyles,makeStyles} from '@material-ui/core/styles';
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableHead from '@material-ui/core/TableHead';
-import TableRow from '@material-ui/core/TableRow';
+import { makeStyles} from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button'
-//import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
 
 const useStyles = makeStyles((theme) => ({
@@ -22,15 +16,6 @@ const useStyles = makeStyles((theme) => ({
  
 }));
 
-const StyledTableCell = withStyles((theme) => ({
-  head: {
-    backgroundColor: theme.palette.common.black,
-    color: theme.palette.common.white,
-  },
-  body: {
-    fontSize: 14,
-  },
-}))(TableCell);
 
 
 
@@ -42,13 +27,8 @@ const StyledTableCell = withStyles((theme) => ({
 
 
 
-const StyledTableRow = withStyles((theme) => ({
-  root: {
-    '&:nth-of-type(odd)': {
-      backgroundColor: theme.palette.action.hover,
-    },
-  },
-}))(TableRow);
+
+
 
 
 
@@ -90,36 +70,6 @@ const classes2 = useStyles();
         </Grid>
     </div>))
   
-    
-   {/* <Table className={classes.table} aria-label="customized table" key={car.id}> 
-     <TableHead >
-          <TableRow>
-            <StyledTableCell>Make</StyledTableCell>
-            <StyledTableCell align="right">Model</StyledTableCell>
-            <StyledTableCell align="right">Origin</StyledTableCell>
-            <StyledTableCell align="right">Year</StyledTableCell>
-            <StyledTableCell align="right">Type</StyledTableCell>
-            <StyledTableCell align="right">Image</StyledTableCell>
-            <StyledTableCell align="right">Delete</StyledTableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody >
-            <StyledTableRow >
-              <StyledTableCell component="th" scope="row">
-                {car.Make}
-              </StyledTableCell>
-              <StyledTableCell align="right">{car.Model}</StyledTableCell>
-              <StyledTableCell align="right">{car.Origin}</StyledTableCell>
-              <StyledTableCell align="right">{car.Year}</StyledTableCell>
-              <StyledTableCell align="right"><p>{car.Type}</p></StyledTableCell>
-              <StyledTableCell align="right"><img src={car.image} alt="car" width="193" height="130"/></StyledTableCell>
-               <StyledTableCell align="right"> <Button variant="contained" color="secondary"  onClick={(e) => (
-                 handleDelete(car.id)
-               )} >Delete</Button ></StyledTableCell>
-            </StyledTableRow>
-        </TableBody>
-               </Table> ));*/
-  }
   
  
   return (
