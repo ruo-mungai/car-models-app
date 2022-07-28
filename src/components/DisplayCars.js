8e062807c69dc734b094f6f19820c0975f565c85
Patrick <ruomungai@gmail.com>
2022-07-28 14:31:37 +0300

    to heruku

--- src/components/DisplayCars.js
+++ src/components/DisplayCars.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { makeStyles} from '@material-ui/core/styles';
-import Button from '@material-ui/core/Button'
+import Button from '@material-ui/core/Button';
 import Grid from '@material-ui/core/Grid';
 
 const useStyles = makeStyles((theme) => ({
